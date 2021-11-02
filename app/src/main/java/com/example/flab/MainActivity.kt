ff6c94258a3457f6d9b5f436de09a39fa1d28a83
daisy-ua <eightnum.81@gmail.com>
2021-11-02 13:17:49 +0200

    Initial commit

---
+++ app/src/main/java/com/example/flab/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.flab
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.flab.ui.theme.FlabTheme
+import org.opencv.android.OpenCVLoader
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            FlabTheme {
+                if (OpenCVLoader.initDebug()) Log.d("rita", "Success")
+                else Log.d("rita", "Failure")
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    FlabTheme {
+        Greeting("Android")
+    }
+}
